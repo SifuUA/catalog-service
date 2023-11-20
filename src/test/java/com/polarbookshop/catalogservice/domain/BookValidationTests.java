654d8d33e728df21639346e9f5e599dade1ccbbb
oleksii <alekseykres@gmail.com>
2023-11-20 06:58:32 +0200

    Add jdbc support

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -22,14 +22,14 @@ public class BookValidationTests {
 
     @Test
     void whenAllFieldsCorrectThenValidationSucceeds() {
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+        var book = Book.of("1234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations.isEmpty());
     }
 
     @Test
     void shouldReturnAnErrorIfIsbnNotCorrect() {
-        var book = new Book("a123a123123", "Title", "Author", 12.12);
+        var book = Book.of("a123a123123", "Title", "Author", 12.12);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).hasSize(1);
         assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
